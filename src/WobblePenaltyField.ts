bceeea0ee0a2b3c611004f25bd89344a4884113a
Philip Johnson <p.zhao.johnson@gmail.com>
2026-03-28 12:46:22 -0400

    Add note explaining what wobble penalty is

--- src/WobblePenaltyField.ts
+++ src/WobblePenaltyField.ts
@@ -1,11 +1,17 @@
+import * as styles from './WobblePenaltyField.module.css';
+
 import { NumberField } from './NumberField';
 
 import { TextInput } from './TextInput';
 
 export class WobblePenaltyField {
+  readonly domNode = document.createElement('div');
+
   readonly #input;
 
   readonly #field;
+
+  readonly #wobblePenaltyNote = new WobblePenaltyNote();
 
   constructor() {
     this.#input = new TextInput();
@@ -13,13 +19,13 @@ export class WobblePenaltyField {
     // default value
     this.#input.domNode.value = '-0.5';
 
-    this.#field = new NumberField('Wobble penalty', this.#input.domNode);
+    this.#field = new NumberField('Wobble penalty *', this.#input.domNode);
+
+    this.domNode.append(this.#field.domNode);
+
+    this.domNode.append(this.#wobblePenaltyNote.domNode);
 
     this.domNode.style.marginTop = '9px';
-  }
-
-  get domNode() {
-    return this.#field.domNode;
   }
 
   get value() {
@@ -32,5 +38,15 @@ export class WobblePenaltyField {
 
   unhide(): void {
     this.domNode.style.visibility = '';
+  }
+}
+
+class WobblePenaltyNote {
+  readonly domNode = document.createElement('p');
+
+  constructor() {
+    this.domNode.classList.add(styles['wobble-penalty-note']);
+
+    this.domNode.textContent = '* For G:U and G:T pairs.';
   }
 }
